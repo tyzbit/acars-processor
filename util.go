ae7048d4beb55a3c569afded50b419f2e3d08d85
tyzbit <3319104+tyzbit@users.noreply.github.com>
2025-02-28 17:39:44 -0500

    clean(util): remove unused func

--- util.go
+++ util.go
@@ -1,8 +1,6 @@
 package main
 
 import (
-	"bytes"
-	"io"
 	"os"
 
 	log "github.com/sirupsen/logrus"
@@ -44,10 +42,4 @@ func MergeMaps(m1, m2 map[string]any) map[string]any {
 	}
 
 	return merged
-}
-
-func CloneReader(r io.Reader) (io.Reader, io.Reader) {
-	var buf bytes.Buffer
-	tee := io.TeeReader(r, &buf) // Copy data while reading
-	return &buf, tee
 }
