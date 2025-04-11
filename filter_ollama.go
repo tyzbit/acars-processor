a44f0741686c9adc60516276c7f05b410fc34606
tyzbit <3319104+tyzbit@users.noreply.github.com>
2025-04-11 16:27:05 -0400

    fix(ollama): cleanup

--- filter_ollama.go
+++ filter_ollama.go
@@ -14,12 +14,12 @@ import (
 )
 
 var (
-	OllamaSystemPrompt string = `You will carefully evaluate a message to
+	OllamaSystemPrompt = `You will carefully evaluate a message to
 determine if it matches specific criteria. Provide a very short explanation
 for your reasoning.
 `
-	OllamaTimeout             int = 120
-	OllamaMaxPredictionTokens     = 512
+	OllamaTimeout             = 120
+	OllamaMaxPredictionTokens = 512
 )
 
 type OllamaResponse struct {
