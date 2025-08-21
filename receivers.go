2b9b518d16b4546565e1816a9ce6d715321a8b33
Tyler Hawkins <3319104+tyzbit@users.noreply.github.com>
2025-08-20 22:50:02 -0400

    breaking: specify individual steps of filter, annotator or receiver in the config

--- receivers.go
+++ receivers.go
@@ -1,22 +1,20 @@
 package main
 
-import log "github.com/sirupsen/logrus"
-
-func ConfigureReceivers() {
-	// Add receivers based on what's enabled
-	if config.Receivers.Webhook.Enabled {
-		log.Info(Success("Webhook receiver enabled"))
-		enabledReceivers = append(enabledReceivers, WebhookHandlerReciever{})
+func (r ReceiverStep) Send(m APMessage) error {
+	receivers := []Receiver{
+		r.Discord,
+		r.NewRelic,
+		r.Webhook,
 	}
-	if config.Receivers.NewRelic.Enabled {
-		log.Info(Success("New Relic reciever enabled"))
-		enabledReceivers = append(enabledReceivers, NewRelicHandlerReciever{})
+	var err error
+	for _, a := range receivers {
+		if !a.Configured() {
+			continue
+		}
+		err = a.Send(m)
+		if err != nil {
+			return err
+		}
 	}
-	if config.Receivers.DiscordWebhook.Enabled {
-		log.Info(Success("Discord reciever enabled"))
-		enabledReceivers = append(enabledReceivers, DiscordHandlerReciever{})
-	}
-	if len(enabledReceivers) == 0 {
-		log.Warn(Attention("no receivers are enabled"))
-	}
+	return nil
 }
